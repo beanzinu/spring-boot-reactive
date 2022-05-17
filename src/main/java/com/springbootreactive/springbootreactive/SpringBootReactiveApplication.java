80a8a598bad0c624e9f49d97dd4e2a9aef87cf88
jinwoo <wlsdn1372@naver.com>
2022-05-17 21:53:57 +0900

    add - 스프링부트 개발자 도구 추가

--- src/main/java/com/springbootreactive/springbootreactive/SpringBootReactiveApplication.java
+++ src/main/java/com/springbootreactive/springbootreactive/SpringBootReactiveApplication.java
@@ -2,11 +2,15 @@ package com.springbootreactive.springbootreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringBootReactiveApplication {
+	// 블록하운드
 
 	public static void main(String[] args) {
+		BlockHound.install();
+
 		SpringApplication.run(SpringBootReactiveApplication.class, args);
 	}
 
